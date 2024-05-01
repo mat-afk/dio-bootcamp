e36bfec73f517cae99e9389d40e279d1d3c167b2
Mateus <mateusc.ramos@outlook.com>
2024-05-01 16:59:12 -0300

    feat: sistema bancario v2 finished

--- challenges/sistema_bancario_v2.py
+++ challenges/sistema_bancario_v2.py
@@ -68,7 +68,6 @@ def criar_usuario(usuarios):
         print(erro("Já existe um usuário com esse CPF"))
         return usuario
 
-    
     nome = input("Informe seu nome: ")
     data_nascimento = input("Informe sua data de nascimento (dd/mm/aaaa): ")
     bairro, logradouro, numero = input("Informe seu endereço (bairro, logradouro, 000): ").split(", ")
@@ -78,13 +77,7 @@ def criar_usuario(usuarios):
         "cpf": cpf,
         "nome": nome,
         "data_nascimento": data_nascimento,
-        "endereco": {
-            "logradouro": logradouro,
-            "numero": numero,
-            "bairro": bairro,
-            "cidade": cidade,
-            "UF": uf
-        }
+        "endereco": f"{logradouro}, {numero} - {bairro}, {cidade}/{uf}"
     }
 
     usuarios.append(novo_usuario)
@@ -100,6 +93,17 @@ def criar_conta(contas, agencia, numero):
 
     if not usuario:
         return print(erro("Não foi encontrado um usuário com esse CPF"))
+    
+    contas_existentes = get_contas_from_usuario(usuario, contas)
+    if contas_existentes:
+        print("Você tem as seguintes contas cadastradas: ")
+        listar_contas(contas_existentes, usuario)
+
+        numero_conta_selecionada = input("Digite o número da conta que você quer usar ou 0 para criar uma nova: ")
+
+        if numero_conta_selecionada != "0":
+            print(sucesso("Nova conta selecionada!"))
+            return get_conta_from_list(numero_conta_selecionada, contas)
     
     nova_conta = {
         "agencia": agencia,
@@ -117,8 +121,29 @@ def criar_conta(contas, agencia, numero):
 
 
 def get_usuario_from_list(cpf: str, usuarios: list):
-    return [usuario for usuario in usuarios if usuario["cpf"] == cpf].pop() if usuarios else None
+    usuario = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuario.pop() if usuario else None
 
+def get_conta_from_list(numero, contas: list):
+    conta = [conta for conta in contas if conta["numero"] == numero]
+    return conta.pop() if conta else None
+
+def get_contas_from_usuario(usuario: dict, contas: list):
+    return [conta for conta in contas if conta["cpf_usuario"] == usuario["cpf"]]
+
+def get_usuario_from_conta(conta, usuarios):
+    return [usuario for usuario in usuarios if conta["cpf_usuario"] == usuario["cpf"]].pop() if usuarios and conta else None
+
+def listar_contas(contas, usuario):
+    if not contas:
+        print(erro("Você não tem contas cadastradas"))
+
+    for conta in contas:
+        print(boilerplate_line("-"))
+        print(f"Agência: {conta["agencia"]}")
+        print(f"C/C: {conta["numero"]}")
+        print(f"Titular: {usuario["nome"]}")
+        print(boilerplate_line("-"))
 
 def get_cpf() -> str:
     cpf = str(input("Digite seu CPF: ")).strip()
@@ -129,6 +154,23 @@ def get_cpf() -> str:
     return cpf.replace(".", "").replace("-", "")
 
 
+def has_usuario_atual(usuario_atual):
+    if not usuario_atual:
+        print(erro("Crie ou selecione um usuário para continuar"))
+        return False
+    
+    return True
+
+def has_conta_atual(conta_atual):
+    if not conta_atual:
+        print(erro("Crie ou selecione uma conta para continuar"))
+        return False
+    
+    return True
+
+def can_do_operations(usuario_atual, conta_atual):
+    return has_usuario_atual(usuario_atual) and has_conta_atual(conta_atual)
+
 # aux design functions
 
 def boilerplate_line(placeholder: str = "-", text: str = "") -> str:
@@ -136,10 +178,8 @@ def boilerplate_line(placeholder: str = "-", text: str = "") -> str:
     text = " " + text + " " if text else text
     return text.center(TERMINAL_COLUMNS, placeholder)
 
-
 def erro(message: str = "Houve um erro"):
     return boilerplate_line("@", "ERRO") + "\n" + boilerplate_line("@", message)
-
 
 def sucesso(message: str = "Sucesso!"):
     return boilerplate_line("=", "SUCESSO") + "\n" + boilerplate_line("=", message)
@@ -149,8 +189,7 @@ menu = f'''
 {boilerplate_line("-")}
 {boilerplate_line(" ", "BEM-VINDO AO SISTEMA BANCÁRIO")}
 {boilerplate_line("-")}
-
-Selecione uma opção para continuar:'''
+'''
 
 options = {
     "1": "novo usuário",
@@ -158,6 +197,7 @@ options = {
     "3": "sacar",
     "4": "depositar",
     "5": "extrato",
+    "6": "listar contas",
     "0": "sair"
 }
 
@@ -180,6 +220,7 @@ print(menu)
 
 while True:
 
+    print("Selecione uma opção para continuar: ")
     for num, operation in options.items():
         print(f"[{num}] {operation.title()}")
 
@@ -189,8 +230,11 @@ while True:
         print(erro("Selecione uma opção válida!"))
 
     elif options[selected_option] == "sacar":
+        if not can_do_operations(usuario_atual, conta_atual):
+            continue
+
         valor = float(input("Digite o valor que você quer sacar: R$ "))
-        saldos, extrato = sacar(
+        saldo, extrato = sacar(
             saldo=saldo,
             valor=valor,
             extrato=extrato,
@@ -200,18 +244,31 @@ while True:
         )
 
     elif options[selected_option] == "depositar":
+        if not can_do_operations(usuario_atual, conta_atual):
+            continue
+
         valor = float(input("Digite o valor que você quer depositar: R$ "))
         saldo, extrato = depositar(saldo, valor, extrato)
 
     elif options[selected_option] == "extrato":
+        if not can_do_operations(usuario_atual, conta_atual):
+            continue
+
         exibir_extrato(saldo, extrato=extrato)
 
     elif options[selected_option] == "novo usuário":
         usuario_atual = criar_usuario(usuarios)
 
     elif options[selected_option] == "nova conta":
-        numero = len(contas) + 1
-        criar_conta(contas, AGENCIA, numero)
+        numero = str(len(contas) + 1)
+        conta_atual = criar_conta(contas, AGENCIA, numero)
+
+    elif options[selected_option] == "listar contas":
+        if not can_do_operations(usuario_atual, conta_atual):
+            continue
+        
+        contas_usuario = get_contas_from_usuario(usuario_atual, contas)
+        listar_contas(contas_usuario, usuario_atual)
 
     elif options[selected_option] == "sair":
         print("Obrigado por usar o nosso sistema!!! :)")
